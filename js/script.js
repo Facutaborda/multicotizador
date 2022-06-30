f87d9589e8d2d98f1ba29181c03784af97438e13
Facundo Taborda <orgcentro.facu@gmail.com>
2022-06-30 17:49:51 -0300

    Se cargan dos secciones mas y se le brinda estilos con SASS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* let nombreUsuario = prompt ("Ingresá tu nombre");
+let edadUsuario = parseInt(prompt ("Ingresá tu edad"));
+let edadEn5 = edadUsuario+5;
+
+alert('Bienvenido/a, ' + nombreUsuario);
+alert('En 5 años tendras ' +edadEn5+ ' años.' );
+ */

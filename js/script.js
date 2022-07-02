5008a0156ff762dc533752dce6889c9604e44a80
Facundo Taborda <orgcentro.facu@gmail.com>
2022-07-02 15:07:06 -0300

    se termino slide de compania, se empieza preguntas frecuentes

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* let nombreUsuario = prompt ("Ingresá tu nombre");
-let edadUsuario = parseInt(prompt ("Ingresá tu edad"));
-let edadEn5 = edadUsuario+5;
-
-alert('Bienvenido/a, ' + nombreUsuario);
-alert('En 5 años tendras ' +edadEn5+ ' años.' );
- */
